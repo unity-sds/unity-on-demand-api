782ca5453af02e95fc6f93b96c95952357c1bc08
Gerald Manipon <pymonger@gmail.com>
2023-05-22 11:03:09 -0700

    use bash to install miniconda; fix indents

--- terraform-unity/deploy.sh
+++ terraform-unity/deploy.sh
@@ -4,7 +4,7 @@ set -ex
 # install conda
 curl -sSL https://repo.anaconda.com/miniconda/Miniconda3-py39_22.11.1-1-Linux-x86_64.sh -o /tmp/install_miniconda.sh
 chmod 755 /tmp/install_miniconda.sh
-/tmp/install_miniconda.sh -b -p /usr/local -u
+bash /tmp/install_miniconda.sh -b -p /usr/local -u
 
 # install serverless
 npm install -g serverless
@@ -20,7 +20,7 @@ cd unity-on-demand-api
 serverless plugin install -n serverless-python-requirements
 serverless plugin install -n serverless-wsgi
 serverless deploy --param cluster_name=unity-sps-on-demand-${RandomStringResource.RandomString} \
-                                        --param sps_api_url=http://${!sps_api}:5002 \
-                                        --param permissionsBoundaryPolicyName=${PrivilegedPolicyName} \
-                                        --region ${AWS::Region} \
-                                        --stage ${RandomStringResource.RandomString}
+  --param sps_api_url=http://${!sps_api}:5002 \
+  --param permissionsBoundaryPolicyName=${PrivilegedPolicyName} \
+  --region ${AWS::Region} \
+  --stage ${RandomStringResource.RandomString}
