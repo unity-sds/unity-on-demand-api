ec48476b5c57dea1762a43db78427543b495d2ef
Gerald Manipon <pymonger@gmail.com>
2023-05-22 12:30:16 -0700

    comment out AWS cred related vars and their usage

--- terraform-unity/main.tf
+++ terraform-unity/main.tf
@@ -16,12 +16,13 @@ resource "docker_container" "serverless" {
   name       = "terraform-docker-test"
   must_run   = false
   entrypoint = ["/bin/bash"]
-  env = [
-    "AWS_ACCESS_KEY_ID=${var.aws_access_key_id}",
-    "AWS_SECRET_ACCESS_KEY=${var.aws_secret_access_key}",
-    "AWS_SESSION_TOKEN=${var.aws_session_token}",
-    "AWS_DEFAULT_REGION=${var.region}"
-  ]
+  # NOTE: not needed when deploying from deployment EC2 instance; remove in the future
+  #env = [
+  #  "AWS_ACCESS_KEY_ID=${var.aws_access_key_id}",
+  #  "AWS_SECRET_ACCESS_KEY=${var.aws_secret_access_key}",
+  #  "AWS_SESSION_TOKEN=${var.aws_session_token}",
+  #  "AWS_DEFAULT_REGION=${var.region}"
+  #]
   command = [
     "/tmp/deploy.sh"
   ]
--- terraform-unity/variables.tf
+++ terraform-unity/variables.tf
@@ -3,26 +3,27 @@ variable "deploy_script_path" {
   type        = string
 }
 
-variable "aws_access_key_id" {
-  description = "AWS access key ID"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_secret_access_key" {
-  description = "AWS secret access key"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_session_token" {
-  description = "AWS session token"
-  type        = string
-  sensitive   = true
-}
-
-variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-west-2"
-}
+# NOTE: not needed when deploying from deployment EC2 instance; remove in the future
+#variable "aws_access_key_id" {
+#  description = "AWS access key ID"
+#  type        = string
+#  sensitive   = true
+#}
+#
+#variable "aws_secret_access_key" {
+#  description = "AWS secret access key"
+#  type        = string
+#  sensitive   = true
+#}
+#
+#variable "aws_session_token" {
+#  description = "AWS session token"
+#  type        = string
+#  sensitive   = true
+#}
+#
+#variable "region" {
+#  description = "AWS region"
+#  type        = string
+#  default     = "us-west-2"
+#}
